3f001957f09b8fe5ce0a3a1f9eb7f6d073eb7a0e
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2019-09-26 21:07:39 +0300

    Initial Commit

---
+++ rdn-trck-cli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  rdn-trck-cli
+//
+//  Created by Dmitriy Trifonov on 26/09/2019.
+//  Copyright © 2019 mitya1234. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
