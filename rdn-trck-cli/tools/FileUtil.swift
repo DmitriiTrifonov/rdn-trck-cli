9bf404c8baa2080b384ca9f2823f78826a439834
Dmitrii Trifonov <53140442+DmitriiTrifonov@users.noreply.github.com>
2019-10-02 12:11:50 +0300

    file utils refactoring

--- rdn-trck-cli/tools/FileUtil.swift
+++ rdn-trck-cli/tools/FileUtil.swift
@@ -10,28 +10,29 @@ import Foundation
 
 
 class FileUtil {
+    static let fileManager = FileManager.default
+    static let filename = "bkshlf.json"
+    static let appFolder = "com.mitya1234.rnd-trck-cli"
+    static let cachesURL = try! fileManager.url(for: .cachesDirectory, in: .localDomainMask, appropriateFor: nil, create: true)
+    static let folderURL = cachesURL.appendingPathComponent(appFolder, isDirectory: true)
+    static let fileURL = folderURL.appendingPathComponent(filename, isDirectory: false)
+    
     static func loadFile() -> Data? {
-        let path = "/Library/Caches/com.mitya1234.rnd-trck-cli/bkshlf.json"
-        let fm = FileManager()
-        if (fm.fileExists(atPath: path)) {
-            let fh = FileHandle(forReadingAtPath: path)?.readDataToEndOfFile()
-            return fh
+        var isDir : ObjCBool = false
+        if (fileManager.fileExists(atPath: folderURL.path, isDirectory: &isDir)) {
+            if (isDir.boolValue) {
+                if (fileManager.fileExists(atPath: fileURL.path)) {
+                    return Data(contentsOf: fileURL)
+                } else {
+                    return nil
+                }
+            }
         } else {
             return nil
         }
     }
     
     static func saveFile(json: Data) {
-        let fileManager = FileManager.default
-        let filename = "bkshlf.json"
-        let appFolder = "com.mitya1234.rnd-trck-cli"
-        
-        let cachesURL = try! fileManager.url(for: .cachesDirectory, in: .localDomainMask, appropriateFor: nil, create: true)
-        //print(cachesURL.path)
-        let folderURL = cachesURL.appendingPathComponent(appFolder, isDirectory: true)
-        //print(folderURL.path)
-        let fileURL = folderURL.appendingPathComponent(filename, isDirectory: false)
-        //print(fileURL.path)
         var isDir : ObjCBool = false
         if (fileManager.fileExists(atPath: folderURL.path, isDirectory: &isDir)) {
             if (isDir.boolValue) {
